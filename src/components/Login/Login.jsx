18ae4349f1b81067449c303e5aba24eb9352b5cc
Neaz-mq <neazmorshed666@gmail.com>
2023-11-25 09:35:31 +0600

    added register page

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,78 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../Hook/AuthProvider";
+
+
+
+const Login = () => {
+    const { googleSignIn, logInUser } = useContext(AuthContext);
+    const handleGoogleLogin = () => {
+      googleSignIn().then((result) => console.log(result.user));
+  
+    };
+      const handleLogin = e => {
+          e.preventDefault();
+          const form = e.target;
+          const email = form.email.value;
+          const password = form.password.value;
+          console.log(email, password);
+          
+          logInUser(email, password)
+          .then(result => {
+            console.log(result.user);
+    
+          })
+          .catch(error => {
+            console.error(error);
+          })
+         
+      }
+  
+   
+    return (
+       
+
+        <div className="hero bg-gradient-to-r from-indigo-500">
+        <div className="hero-content flex-col lg:flex-row-reverse w-2/3  ">
+        <form  onSubmit={handleLogin}> 
+          <div className="card flex-shrink-0 w-full  max-w-sm shadow-2xl bg-base-300 px-6 py-7">
+            <h2 className="lg:text-3xl text-xl py-5 text-center font-bold">Please Login</h2>
+            <div>
+
+
+              <div className="form-control w-36">
+                <label className="label">
+                  <span className="label-text text-xl font-semibold">Email</span>
+                </label>
+                <input 
+                  className="form-controls w-40 lg:w-72 md:w-56 rounded-md p-2"
+                  type="email" name="email"
+                  placeholder=" Type Email" />
+              </div>
+
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text text-xl font-semibold">Password</span>
+                </label>
+                <input 
+                  className="form-control w-40 lg:w-72 md:w-56 rounded-md p-2"
+                  type="password" name="password"
+                  placeholder=" Type Password" />
+
+              </div>
+              <div className="form-control mt-6">
+                <button   className="btn btn-primary w-1/3">Login</button>
+                <br />
+                <button onClick={handleGoogleLogin}  className="btn btn-secondary"> Google Login</button>
+              </div>
+              <p className="mt-4 pb-8 lg:text-xl font-semibold">Don't have an account! Please <Link className="text-red-600 font-bold" to="/register">Register</Link></p>
+            </div>
+          </div>
+          </form>
+        </div>
+        </div>
+      
+    );
+};
+
+export default Login;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 
 import {
@@ -9,15 +8,28 @@ import {
 } from "react-router-dom";
 import Root from './components/Root/Root.jsx';
 import Home from './components/Home/Home.jsx';
+import ErrorPage from './components/ErrorPage/ErrorPage.jsx';
+import Register from './components/Register/Register.jsx';
+import Login from './components/Login/Login.jsx';
+import AuthProvider from './Hook/AuthProvider.jsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
     children: [
       {
         path: '/',
         element: <Home></Home>
+      },
+      {
+        path: '/register',
+        element: <Register></Register>,
+      },
+      {
+        path: '/login',
+        element: <Login></Login>,
       },
     ]
   },
@@ -25,6 +37,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   <RouterProvider router={router} />
+  <AuthProvider>
+  <RouterProvider router={router} />
+  </AuthProvider>
   </React.StrictMode>,
 )
