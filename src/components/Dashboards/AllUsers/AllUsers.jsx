a87aa55feb81d426a7c122336fb559e323c84b28
Neaz-mq <neazmorshed666@gmail.com>
2023-11-27 20:13:16 +0600

    added all user data show in tabular form

--- src/components/Dashboards/AllUsers/AllUsers.jsx
+++ src/components/Dashboards/AllUsers/AllUsers.jsx
@@ -1,6 +1,15 @@
+import { useEffect, useState } from "react";
+import User from "../../User/User";
 
 
 const AllUsers = () => {
+    const [user, setUser] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/user')
+            .then(res => res.json())
+            .then(data => setUser(data));
+    }, [])
+
     return (
         <div>
             <h1 className="text-4xl text-center my-10">All Users</h1>
@@ -13,25 +22,56 @@ const AllUsers = () => {
                             <th>
                                 ID
                             </th>
-                          
-                            <th>Name</th>
+
+
                             <th>Email</th>
                             <th>Image</th>
-                            <th><button className="btn btn-active btn-primary">Admin</button></th>
-                          
+                            
+
                         </tr>
                     </thead>
                     <tbody>
-                      
+                        {
+                            user.map(user => <tr key={user._id}>
+                                <th>
+                                <td>
+                                    {user._id}
+                                   
+                                </td>
+                                </th>
+                                
+                                <td>
+                                    {user.email}
+                                   
+                                </td>
 
+                                <td>
+                                    <div className="flex items-center gap-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-12 h-12">
+                                                <img src="https://i.ibb.co/Vg80T5P/profile.png" alt="Avatar Tailwind CSS Component" />
+                                            </div>
+                                        </div>
+                                        
+                                    </div>
+                                </td>
+                                <td>
+                                <button className="btn btn-active btn-primary"> Make Admin</button>
+                                   
+                                </td>
+                              
+                            </tr>)
+                        }
+                        
 
                     </tbody>
                 </table>
             </div>
         </div>
 
-        
+
     );
 };
 
 export default AllUsers;
+
